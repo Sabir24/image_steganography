ec8d32a6f7589c97db53c687d36be6ece020aea0
G Saber Hussain <117213190+Sabir24@users.noreply.github.com>
2023-12-01 20:47:52 +0530

    Image_Steganography

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String arg[]) {
+    
+//    RegisterForm b = new RegisterForm();
+    
+    SplashScreenFrame a=new SplashScreenFrame();
+    a.setVisible(true);
+    a.setLocationRelativeTo(null);
+    
+//    RegisterForm b = new RegisterForm();
+//    b.setVisible(true);
+//    b.setLocationRelativeTo(null);
+    }
+}
